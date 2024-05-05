802d41ab6568ca6396c8dbac0d28ae89c5ca14be
Si Lam <silam@hotmail.com>
2024-05-05 09:24:34 -0500

    Initial checkedin

---
+++ BlazorSignalRServerApp/Data/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRServerApp.Data
+{
+    public class ChatHub: Hub
+    {
+        public ChatHub() { }
+
+        public async Task SendMessage(string sender, string receiver, string msgTitle, string msgBody)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", sender, receiver, msgTitle, msgBody);
+        }
+    }
+}
---
+++ BlazorSignalRServerApp/Data/NotificationMessage.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorSignalRServerApp.Data
+{
+    public class NotificationMessage
+    {
+        public NotificationMessage() { }
+        public int MsgId { get; set; }
+        public string SenderName { get; set; }
+
+        public string ReceiverName { get; set; }
+
+        public string MsgTitle { get; set; }
+
+
+        public string MsgBody { get; set; }
+
+        public DateTime MsgDate { get; set; } = DateTime.Now;
+
+        public string MsgDateStr => this.MsgDate.ToString("dd-MMM-yyyy");
+
+
+
+
+    }
+}
